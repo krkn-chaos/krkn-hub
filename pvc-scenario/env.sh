6024be06dacf2594e11bace40053bc0f13f16292
Alejandro Gullón <agullon@redhat.com>
2021-11-30 17:11:27 +0100

    adding files for pvc_scenario

---
+++ pvc-scenario/env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Vars and respective defaults
+export PVC_NAME=${PVC_NAME:=""}
+export POD_NAME=${POD_NAME:=""}
+export NAMESPACE=${NAMESPACE:=<namespace>}
+export FILL_PERCENTAGE=${FILL_PERCENTAGE:="50"}
+export DURATION=${DURATION:="60"}
+export SCENARIO_TYPE=${SCENARIO_TYPE:=pvc_scenarios}
+export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/pvc_scenario.yaml}
+
---
+++ pvc-scenario/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+# Source env.sh to read all the vars
+source /root/main_env.sh
+source /root/env.sh
+
+ls -la /root/.kube
+
+source /root/common_run.sh
+checks
+config_setup
+
+# Substitute config with environment vars defined
+envsubst < /root/kraken/scenarios/pvc_scenario.yaml.template > /root/kraken/scenarios/pvc_scenario.yaml
+envsubst < /root/kraken/config/config.yaml.template > /root/kraken/config/config.yaml
+
+# Run Kraken
+cd /root/kraken
+
+
+python3 run_kraken.py --config config/config.yaml
