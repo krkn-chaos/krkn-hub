feec399012243ba4f115ece7a083735b3f928dc5
Alejandro Gullón <agullon@redhat.com>
2021-12-01 12:00:12 +0100

    adding readme files and ci tests

---
+++ CI/tests/test_pvc_scenario.sh
@@ -0,0 +1,31 @@
+set -xo pipefail
+
+source CI/tests/common.sh
+
+command="docker"
+#command=$(command_to_run)
+#echo "final $command"
+#if [ "$command" == "" ]
+#then
+#  echo "please install podman or docker before continuing"
+#  exit 1
+#fi
+
+function functional_test_pvc_scenario {  
+  export PVC_NAME=kraken-test-pvc
+  export POD_NAME=kraken-test-pod
+  export NAMESPACE=default
+
+  container_name="pvc_scenario_test"
+  $command-compose build pvc-scenario
+  . ./get_docker_params.sh
+  image_id=$($command images --format "{{.ID}}" | head -n 1)
+  $command run --name=$container_name --net=host $PARAMS -v /root/.kube/config:/root/.kube/config:Z -d $image_id
+  $command logs -f $container_name
+  final_exit_code=$(get_exit_code $command $container_name)
+  echo "exit code final: $final_exit_code"
+  delete_containers_and_images $command
+  exit $final_exit_code
+}
+
+functional_test_pvc_scenario
--- pvc-scenario/env.sh
+++ pvc-scenario/env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Vars and respective defaults
-export PVC_NAME=${PVC_NAME:=""}
-export POD_NAME=${POD_NAME:=""}
+export PVC_NAME=${PVC_NAME:=<pvc_name>}
+export POD_NAME=${POD_NAME:=<pod_name>}
 export NAMESPACE=${NAMESPACE:=<namespace>}
 export FILL_PERCENTAGE=${FILL_PERCENTAGE:="50"}
 export DURATION=${DURATION:="60"}
