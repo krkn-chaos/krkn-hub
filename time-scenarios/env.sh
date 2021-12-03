9ac3a508332f6076796a1c2ba1f1a6a7aa32ae50
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2021-12-03 08:43:11 -0500

    Add additional parameters to time scenarios

--- time-scenarios/env.sh
+++ time-scenarios/env.sh
@@ -6,6 +6,7 @@ export OBJECT_TYPE=${OBJECT_TYPE:="pod"}
 export LABEL_SELECTOR=${LABEL_SELECTOR:="k8s-app=etcd"}
 export OBJECT_NAME=${OBJECT_NAME:=""}
 export NAMESPACE=${NAMESPACE:=""}
+export CONTAINER_NAME=${CONTAINER_NAME:=""}
 export SCENARIO_TYPE=${SCENARIO_TYPE:=time_scenarios}
 export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/time_scenario.yaml}
 export SCENARIO_POST_ACTION=${SCENARIO_POST_ACTION:=""}
