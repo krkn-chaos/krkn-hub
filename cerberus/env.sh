02c302c182ec1b66396d2d2d4266e7d9a9c7f8d2
Paige Rubendall <prubenda@redhat.com>
2022-03-02 12:49:09 -0500

    Adding missing parameters to docs and inpsect components param

--- cerberus/env.sh
+++ cerberus/env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Vars and respective defaults
+export CERBERUS_KUBECONFIG=/root/.kube/config
 export CERBERUS_PORT=${CERBERUS_PORT:=8080}
 export CERBERUS_WATCH_NODES=${CERBERUS_WATCH_NODES:=True}
 export CERBERUS_WATCH_OPERATORS=${CERBERUS_WATCH_OPERATORS:=True}
 export CERBERUS_WATCH_NAMESPACES=${CERBERUS_WATCH_NAMESPACES:=[openshift-etcd, openshift-apiserver, openshift-kube-apiserver, openshift-monitoring, openshift-kube-controller-manager, openshift-machine-api, openshift-kube-scheduler, openshift-ingress, openshift-sdn]}
 export CERBERUS_TIMEOUT=${CERBERUS_TIMEOUT:=3}
+export CERBERUS_SLEEP=${CERBERUS_SLEEP:=3}
 export CERBERUS_CORES=${CERBERUS_CORES:=1}
 export CERBERUS_ITERATIONS=${CERBERUS_ITERATIONS:=5}
 export CERBERUS_DAEMON_MODE=${CERBERUS_DAEMON_MODE:=True}
+export INSPECT_COMPONENTS=${INSPECT_COMPONENTS:=False}
