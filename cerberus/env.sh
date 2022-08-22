f61363b5ad9c64a47e49da14053a7d25ab770bf4
Paige Rubendall <prubenda@redhat.com>
2022-08-22 14:20:31 -0400

    aligning cerberus cores default from cerberus in krkn-hub

--- cerberus/env.sh
+++ cerberus/env.sh
@@ -8,7 +8,7 @@ export CERBERUS_WATCH_OPERATORS=${CERBERUS_WATCH_OPERATORS:=True}
 export CERBERUS_WATCH_NAMESPACES=${CERBERUS_WATCH_NAMESPACES:=[openshift-etcd, openshift-apiserver, openshift-kube-apiserver, openshift-monitoring, openshift-kube-controller-manager, openshift-machine-api, openshift-kube-scheduler, openshift-ingress, openshift-sdn]}
 export CERBERUS_TIMEOUT=${CERBERUS_TIMEOUT:=3}
 export CERBERUS_SLEEP=${CERBERUS_SLEEP:=3}
-export CERBERUS_CORES=${CERBERUS_CORES:=1}
+export CERBERUS_CORES=${CERBERUS_CORES:=.5}
 export CHUNK_SIZE=${CHUNK_SIZE:=100}
 export CERBERUS_ITERATIONS=${CERBERUS_ITERATIONS:=5}
 export CERBERUS_DAEMON_MODE=${CERBERUS_DAEMON_MODE:=True}
