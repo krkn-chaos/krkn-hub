0d095063a771f87983d11f6e1efbd82cca0bbba6
Harshil Sabhnani <hsabhnan@redhat.com>
2022-08-23 16:18:53 -0400

    Syntax fixes and ability to run without going to github

--- node-cpu-hog/env.sh
+++ node-cpu-hog/env.sh
@@ -2,12 +2,12 @@
 
 # Vars and respective defaults
 export JOB_CLEANUP_POLICY=${JOB_CLEANUP_POLICY:="delete"}
-export LITMUS_INSTALL:=${LITMUS_INSTALL:=true}
-export LITMUS_UNINSTALL_BEFORE_RUN:=${LITMUS_UNINSTALL_BEFORE_RUN:=true}
+export LITMUS_INSTALL=${LITMUS_INSTALL:=true}
+export LITMUS_UNINSTALL_BEFORE_RUN=${LITMUS_UNINSTALL_BEFORE_RUN:=true}
 export TOTAL_CHAOS_DURATION=${TOTAL_CHAOS_DURATION:="300"}
 export NODE_CPU_CORE=${NODE_CPU_CORE:="2"}
 export NODES_AFFECTED_PERC=${NODES_AFFECTED_PERC:=""}
 export TARGET_NODES=${TARGET_NODES:=""}
 export SCENARIO_TYPE=${SCENARIO_TYPE:=litmus_scenarios}
-export SCENARIO_FILE=${SCENARIO_FILE:=- https://raw.githubusercontent.com/cloud-bulldozer/kraken/master/scenarios/templates/litmus-rbac.yaml}
+export SCENARIO_FILE=${SCENARIO_FILE:=- scenarios/templates/litmus-rbac.yaml}
 export SCENARIO_POST_ACTION=${SCENARIO_POST_ACTION:=- scenarios/node_hog_engine.yaml}
--- node-io-hog/env.sh
+++ node-io-hog/env.sh
@@ -2,13 +2,13 @@
 
 # Vars and respective defaults
 export JOB_CLEANUP_POLICY=${JOB_CLEANUP_POLICY:="delete"}
-export LITMUS_INSTALL:=${LITMUS_INSTALL:=true}
-export LITMUS_UNINSTALL_BEFORE_RUN:=${LITMUS_UNINSTALL_BEFORE_RUN:=true} 
+export LITMUS_INSTALL=${LITMUS_INSTALL:=true}
+export LITMUS_UNINSTALL_BEFORE_RUN=${LITMUS_UNINSTALL_BEFORE_RUN:=true} 
 export TOTAL_CHAOS_DURATION=${TOTAL_CHAOS_DURATION:="300"}
 export FILESYSTEM_UTILIZATION_PERCENTAGE=${FILESYSTEM_UTILIZATION_PERCENTAGE:="90"}
 export NUMBER_OF_WORKERS=${NUMBER_OF_WORKERS:=""}
 export CPU=${CPU:=2}
 export TARGET_NODES=${TARGET_NODES:=""}
 export SCENARIO_TYPE=${SCENARIO_TYPE:=litmus_scenarios}
-export SCENARIO_FILE=${SCENARIO_FILE:=- https://raw.githubusercontent.com/cloud-bulldozer/kraken/master/scenarios/templates/litmus-rbac.yaml}
+export SCENARIO_FILE=${SCENARIO_FILE:=- scenarios/templates/litmus-rbac.yaml}
 export SCENARIO_POST_ACTION=${SCENARIO_POST_ACTION:=- scenarios/io_hog.yaml}
