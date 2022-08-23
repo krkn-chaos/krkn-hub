aad638577421a45113f48f3a6d5c3d3124513e9a
Harshil Sabhnani <hsabhnan@redhat.com>
2022-08-23 16:18:53 -0400

    Syntax fixes and ability to run without going to github

--- node-memory-hog/env.sh
+++ node-memory-hog/env.sh
@@ -2,11 +2,11 @@
 
 # Vars and respective defaults
 export JOB_CLEANUP_POLICY=${JOB_CLEANUP_POLICY:="delete"}
-export LITMUS_INSTALL:=${LITMUS_INSTALL:=true}
-export LITMUS_UNINSTALL_BEFORE_RUN:=${LITMUS_UNINSTALL_BEFORE_RUN:=true} 
+export LITMUS_INSTALL=${LITMUS_INSTALL:=true}
+export LITMUS_UNINSTALL_BEFORE_RUN=${LITMUS_UNINSTALL_BEFORE_RUN:=true} 
 export TOTAL_CHAOS_DURATION=${TOTAL_CHAOS_DURATION:="300"}
 export MEMORY_CONSUMPTION_PERCENTAGE=${MEMORY_CONSUMPTION_PERCENTAGE:="90"}
 export TARGET_NODES=${TARGET_NODES:=""}
 export SCENARIO_TYPE=${SCENARIO_TYPE:=litmus_scenarios}
-export SCENARIO_FILE=${SCENARIO_FILE:=- https://raw.githubusercontent.com/cloud-bulldozer/kraken/master/scenarios/templates/litmus-rbac.yaml}
+export SCENARIO_FILE=${SCENARIO_FILE:=- scenarios/templates/litmus-rbac.yaml}
 export SCENARIO_POST_ACTION=${SCENARIO_POST_ACTION:=- scenarios/memory_hog.yaml}
