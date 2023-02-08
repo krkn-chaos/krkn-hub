f50b8f4fe405f3cfa272e05b0b3c18397120c70b
Paige Rubendall <prubenda@redhat.com>
2023-02-08 15:24:55 -0500

    Adding kraken location with root

--- prow/pod-scenarios/prow_run.sh
+++ prow/pod-scenarios/prow_run.sh
@@ -8,7 +8,7 @@ ls
 source env.sh
 source pod-scenarios/env.sh
 
-krn_loc=../kraken
+krn_loc=/root/kraken
 
 # Substitute config with environment vars defined
 if [[ -z "$POD_LABEL" ]]; then
@@ -22,4 +22,4 @@ envsubst < config.yaml.template > $krn_loc/config/pod_scenario_config.yaml
 cat $krn_loc/config/pod_scenario_config.yaml
 cat $krn_loc/scenarios/pod_scenario.yaml
 
-python3 $krn_loc/run_kraken.py --config=config/pod_scenario_config.yaml
+python3 $krn_loc/run_kraken.py --config=$krn_loc/config/pod_scenario_config.yaml
