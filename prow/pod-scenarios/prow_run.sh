87676114a3fd28851c7800e40ba3456096743826
Paige Rubendall <prubenda@redhat.com>
2023-03-01 11:35:14 -0500

    adding different folder locations

--- prow/pod-scenarios/prow_run.sh
+++ prow/pod-scenarios/prow_run.sh
@@ -12,14 +12,14 @@ krn_loc=/root/kraken
 
 # Substitute config with environment vars defined
 if [[ -z "$POD_LABEL" ]]; then
-  envsubst < pod-scenarios/pod_scenario_namespace.yaml.template > $krn_loc/scenarios/pod_scenario.yaml
+  envsubst < pod-scenarios/pod_scenario_namespace.yaml.template > pod-scenarios/pod_scenario.yaml
 else  
-  envsubst < pod-scenarios/pod_scenario.yaml.template > $krn_loc/scenarios/pod_scenario.yaml
+  envsubst < pod-scenarios/pod_scenario.yaml.template > pod-scenarios/pod_scenario.yaml
 fi
-export SCENARIO_FILE=${SCENARIO_FILE:=$krn_loc/scenarios/pod_scenario.yaml}
-envsubst < config.yaml.template > $krn_loc/config/pod_scenario_config.yaml
+export SCENARIO_FILE=${SCENARIO_FILE:=pod-scenarios/pod_scenario.yaml}
+envsubst < config.yaml.template > pod_scenario_config.yaml
 
-cat $krn_loc/config/pod_scenario_config.yaml
-cat $krn_loc/scenarios/pod_scenario.yaml
+cat pod_scenario_config.yaml
+cat pod-scenarios/pod_scenario.yaml
 
-python3 $krn_loc/run_kraken.py --config=$krn_loc/config/pod_scenario_config.yaml
+python3 $krn_loc/run_kraken.py --config=pod_scenario_config.yaml
