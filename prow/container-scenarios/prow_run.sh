2d28ceaaca7930dde6e6f1f02a8503b65f381556
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2023-03-20 13:21:51 -0400

    Add prow container scenarios (#82)
    This enables krkn-hub container scenarios integration in prow: https://prow.ci.openshift.org/

---
+++ prow/container-scenarios/prow_run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+ls
+
+# Source env.sh to read all the vars
+source env.sh
+
+export KUBECONFIG=$KRKN_KUBE_CONFIG
+oc version 
+cat $KRKN_KUBE_CONFIG
+oc config view
+source container-scenarios/env.sh
+
+krn_loc=/root/kraken
+
+# Substitute config with environment vars defined
+envsubst < container-scenarios/container_scenario.yaml.template > container-scenarios/container_scenario.yaml
+export SCENARIO_FILE=container-scenarios/container_scenario.yaml
+envsubst < config.yaml.template > container_scenario_config.yaml
+
+# Run Kraken
+cat container_scenario_config.yaml
+cat container-scenarios/container_scenario.yaml
+python3 $krkn_loc/run_kraken.py --config=container_scenario_config.yaml
