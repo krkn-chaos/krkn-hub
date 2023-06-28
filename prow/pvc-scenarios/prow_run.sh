363fe9b35cc1101bec4b18c747b691c9084f9d34
Sahil Shah <shah.sahil264@gmail.com>
2023-06-28 14:22:15 -0400

    Adding actual pvc-scenario to prow

--- prow/pvc-scenarios/prow_run.sh
+++ prow/pvc-scenarios/prow_run.sh
@@ -12,21 +12,22 @@ export KUBECONFIG=$KRKN_KUBE_CONFIG
 # cluster details
 echo "Printing cluster details"
 oc version
-cat $KRKN_KUBE_CONFIG
+cat $KRKN_KUBE_CONFIGß
 oc config view
 echo "Printing node info"
 for node in $(oc get nodes | awk 'NR!=1{print $1}'); do oc get node/$node -o yaml; done
 
-source container-scenarios/env.sh
+
+source pvc-scenarios/env.sh
 
 krkn_loc=/root/kraken
 
 # Substitute config with environment vars defined
-envsubst < container-scenarios/container_scenario.yaml.template > container-scenarios/container_scenario.yaml
-export SCENARIO_FILE="- container-scenarios/container_scenario.yaml"
-envsubst < config.yaml.template > container_scenario_config.yaml
+envsubst < pvc-scenarios/pvc_scenario.yaml.template > pvc-scenarios/pvc_scenario.yaml
+export SCENARIO_FILE="- pvc-scenarios/pvc_scenario.yaml"
+envsubst < config.yaml.template > pvc_scenario_config.yaml
 
 # Run Kraken
-cat container_scenario_config.yaml
-cat container-scenarios/container_scenario.yaml
-python3 $krkn_loc/run_kraken.py --config=container_scenario_config.yaml
+cat pvc_scenario_config.yaml
+cat pvc-scenarios/pvc_scenario.yaml
+python3 $krkn_loc/run_kraken.py --config=pvc_scenario_config.yaml
