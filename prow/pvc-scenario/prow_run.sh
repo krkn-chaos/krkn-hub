322cb4c3d693e5a9e4be47e3cbeb022a6384f8b0
Sahil Shah <shah.sahil264@gmail.com>
2023-07-10 12:08:19 -0400

    Adding stderr and stdout

--- prow/pvc-scenario/prow_run.sh
+++ prow/pvc-scenario/prow_run.sh
@@ -24,10 +24,10 @@ krkn_loc=/root/kraken
 
 # Substitute config with environment vars defined
 envsubst < pvc-scenario/pvc_scenario.yaml.template > pvc-scenario/pvc_scenario.yaml
-export SCENARIO_FILE="- pvc-scenario/pvc_scenario.yaml"
+export SCENARIO_FILE="pvc-scenario/pvc_scenario.yaml"
 envsubst < config.yaml.template > pvc_scenario_config.yaml
 
 # Run Kraken
 cat pvc_scenario_config.yaml
 cat pvc-scenario/pvc_scenario.yaml
-python3.9 $krkn_loc/run_kraken.py --config=pvc_scenario_config.yaml
+python3.9 $krkn_loc/run_kraken.py --config=pvc_scenario_config.yaml 2&>1
