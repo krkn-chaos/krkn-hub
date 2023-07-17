ce553538e8c4650a0f650fb132c79f28a0560459
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2023-07-16 23:25:02 -0400

    Setup arca environment for hog scenarios in prow

--- prow/memory-hog/prow_run.sh
+++ prow/memory-hog/prow_run.sh
@@ -8,6 +8,8 @@ ls
 source env.sh
 
 export KUBECONFIG=$KRKN_KUBE_CONFIG
+krkn_loc=/root/kraken
+SCENARIO_FOLDER="$krkn_loc/scenarios/arcaflow/memory-hog"
 
 # cluster details
 echo "Printing cluster details"
@@ -18,8 +20,13 @@ echo "Printing node info"
 for node in $(oc get nodes | awk 'NR!=1{print $1}'); do oc get node/$node -o yaml; done
 
 source node-memory-hog/env.sh
+source main_env.sh
+source env.sh
+source common_run.sh
 
-krkn_loc=/root/kraken
+setup_arcaflow_env "$SCENARIO_FOLDER"
+checks
+config_setup
 
 # Substitute config with environment vars defined
 envsubst < node-memory-hog/input.yaml.template> node-memory-hog/memory_hog_scenario.yaml
