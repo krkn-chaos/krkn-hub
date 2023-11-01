b98ea990e547d38a35df4798fc80eee70e565dfb
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2023-11-01 19:53:04 -0400

    Fix prow node-scenarios config file location

--- prow/node-disruptions/prow_run.sh
+++ prow/node-disruptions/prow_run.sh
@@ -25,12 +25,13 @@ krkn_loc=/tmp/kraken
 
 # Substitute config with environment vars defined
 if [[ "$CLOUD_TYPE" == "vmware" || "$CLOUD_TYPE" == "ibmcloud" ]]; then
-  envsubst < $krkn_loc/scenarios/plugin_node_scenario.yaml.template > $krkn_loc/scenarios/node_scenario.yaml
+  envsubst < node-scenarios/plugin_node_scenario.yaml.template > $krkn_loc/scenarios/node_scenario.yaml
   export SCENARIO_TYPE="plugin_scenarios"
   export ACTION=${ACTION:="$CLOUD_TYPE-node-reboot"}
 else
-  envsubst < $krkn_loc/scenarios/node_scenario.yaml.template > $krkn_loc/scenarios/node_scenario.yaml
+  envsubst < node-scenarios/node_scenario.yaml.template > $krkn_loc/scenarios/node_scenario.yaml
 fi
+export SCENARIO_FILE=$krkn_loc/scenarios/node_scenario.yaml
 envsubst < config.yaml.template > node_scenario_config.yaml
 
 # Run Kraken
