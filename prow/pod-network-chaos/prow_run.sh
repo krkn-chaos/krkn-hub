6bd01ce05225eb5a6fd08042d1e327e5dff5f1e3
Sahil Shah <shah.sahil264@gmail.com>
2024-01-02 14:50:27 -0500

    Pod network scenario for prow (#154)
    * Pod network scenario for prow
    
    * adding envsubst
    
    * minor change

---
+++ prow/pod-network-chaos/prow_run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -ex
+
+ls
+
+# Source env.sh to read all the vars
+source env.sh
+
+export KUBECONFIG=$KRKN_KUBE_CONFIG
+
+
+# Cluster details
+echo "Printing cluster details"
+oc version 
+cat $KRKN_KUBE_CONFIG
+oc config view
+echo "Printing node info"
+for node in $(oc get nodes | awk 'NR!=1{print $1}'); do oc get node/$node -o yaml; done
+
+source pod-network-chaos/env.sh
+
+krn_loc=/root/kraken
+
+# Substitute config with environment vars defined
+if [[ -z $NAMESPACE ]]; then
+  echo "Requires NAMASPACE parameter to be set, please check"
+  exit 1
+fi
+envsubst < pod_network_scenario.yaml.template > pod_network_scenario.yaml
+export SCENARIO_FILE=pod-network-chaos/pod_network_scenario.yaml
+envsubst < config.yaml.template > pod_network_scenario_config.yaml
+
+cat pod_network_scenario_config.yaml
+cat pod-network-chaos/pod_network_scenario.yaml
+
+python3.9 $krn_loc/run_kraken.py --config=pod_network_scenario_config.yaml
