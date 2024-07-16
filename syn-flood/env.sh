9ac01870cf86129be32da4de4d990b6ca15bed19
Tullio Sebastiani <tsebasti@redhat.com>
2024-07-16 11:16:27 +0200

    added syn-flood scenario scripts
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    docker file fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix

---
+++ syn-flood/env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export SCENARIO_CONFIG_FILE="$KRAKEN_FOLDER/scenarios/kube/syn_flood_config.yaml"
+export PACKET_SIZE=${PACKET_SIZE:="120"}
+export WINDOW_SIZE=${WINDOW_SIZE:="64"}
+export TOTAL_CHAOS_DURATION=${TOTAL_CHAOS_DURATION:="120"}
+export NAMESPACE=${NAMESPACE:="default"}
+export TARGET_SERVICE=${TARGET_SERVICE}
+export TARGET_PORT=${TARGET_PORT}
+export TARGET_SERVICE_LABEL=${TARGET_SERVICE_LABEL}
+export NUMBER_OF_PODS=${NUMBER_OF_PODS:="2"}
+export IMAGE=${IMAGE:="quay.io/krkn-chaos/krkn-syn-flood"}
+export NODE_SELECTORS=${NODE_SELECTORS:=""}
+
+python3.9 $ROOT_FOLDER/build_config_file.py --outconfig $SCENARIO_CONFIG_FILE
+cat $SCENARIO_CONFIG_FILE
+
+export SCENARIO_TYPE=${SCENARIO_TYPE:=syn_flood}
+export SCENARIO_FILE=${SCENARIO_FILE:=$SCENARIO_CONFIG_FILE}
---
+++ syn-flood/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+ROOT_FOLDER="/home/krkn"
+KRAKEN_FOLDER="$ROOT_FOLDER/kraken"
+SCENARIO_FOLDER="$KRAKEN_FOLDER/scenarios/syn-flood"
+
+# Source env.sh to read all the vars
+source $ROOT_FOLDER/main_env.sh
+source $ROOT_FOLDER/env.sh
+
+source $ROOT_FOLDER/common_run.sh
+
+# Substitute config with environment vars defined
+envsubst < $KRAKEN_FOLDER/config/config.yaml.template > $KRAKEN_FOLDER/config/syn_flood_config.yaml
+
+checks
+config_setup
+
+# Run Kraken
+cd $KRAKEN_FOLDER
+python3.9 run_kraken.py --config=config/syn_flood_config.yaml
+
+
