2fadc70466f9d25cdb20c3ca62e66e6df56ff538
Auto User <auto@users.noreply.github.com>
2024-07-22 12:27:15 -0400

    taking out verify session for ibm cloud
    rh-pre-commit.version: 2.2.0
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Auto User <auto@users.noreply.github.com>

--- node-scenarios/run.sh
+++ node-scenarios/run.sh
@@ -11,9 +11,22 @@ checks
 
 # Substitute config with environment vars defined
 if [[ "$CLOUD_TYPE" == "vmware" || "$CLOUD_TYPE" == "ibmcloud" ]]; then
-  envsubst < /home/krkn/kraken/scenarios/plugin_node_scenario.yaml.template > /home/krkn/kraken/scenarios/node_scenario.yaml
-  export SCENARIO_TYPE="plugin_scenarios"
   export ACTION=${ACTION:="$CLOUD_TYPE-node-reboot"}
+  envsubst < /root/kraken/scenarios/plugin_node_scenario.yaml.template > /root/kraken/scenarios/node_scenario.yaml
+  export SCENARIO_TYPE="plugin_scenarios"
+
+  # IBM doesnt have verify session
+  # Invalid parameter 'verify_session', expected one of: name, runs, label_selector, timeout, instance_count, skip_openshift_checks, kubeconfig_path
+  if [[ "$CLOUD_TYPE" == "vmware" ]]; then
+    
+    ## Set to True if you want to verify the vSphere client session using certificates; else False
+    export VERIFY_SESSION="verify_session: $VERIFY_SESSION"
+  else
+    export VERIFY_SESSION=""
+  fi
+
+
+
 else
   envsubst < /home/krkn/kraken/scenarios/node_scenario.yaml.template > /home/krkn/kraken/scenarios/node_scenario.yaml
 fi
