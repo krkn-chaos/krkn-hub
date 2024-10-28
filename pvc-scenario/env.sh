b3942c7db2931b414f70e0f2de1b1a1d1d45bdb4
Tullio Sebastiani <tsebasti@redhat.com>
2024-10-28 10:20:49 +0100

    added BLOCK_SIZE variable for dd failover
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- pvc-scenario/env.sh
+++ pvc-scenario/env.sh
@@ -8,4 +8,5 @@ export FILL_PERCENTAGE=${FILL_PERCENTAGE:="50"}
 export DURATION=${DURATION:="60"}
 export SCENARIO_TYPE=${SCENARIO_TYPE:=pvc_scenarios}
 export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/pvc_scenario.yaml}
+export BLOCK_SIZE=${BLOCK_SIZE:="102400"}
 
