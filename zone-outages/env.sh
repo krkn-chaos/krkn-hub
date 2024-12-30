01c78c3931878eb80f230ccdfcff58b3b1d57070
Naga Ravi Chaitanya Elluri <nelluri@redhat.com>
2024-12-30 14:53:59 -0500

    Support user provided acl's for zone-outages
    Reference PR in Krkn: https://github.com/krkn-chaos/krkn/pull/731
    
    Signed-off-by: Naga Ravi Chaitanya Elluri <nelluri@redhat.com>

--- zone-outages/env.sh
+++ zone-outages/env.sh
@@ -6,5 +6,6 @@ export CLOUD_TYPE=${CLOUD_TYPE:="aws"}
 export DURATION=${DURATION:=600}
 export VPC_ID=${VPC_ID:=""}
 export SUBNET_ID=${SUBNET_ID:=""}
+export DEFAULT_ACL_ID=${DEFAULT_ACL_ID:=""}
 export SCENARIO_TYPE=${SCENARIO_TYPE:=zone_outages_scenarios}
 export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/zone_outage.yaml}
