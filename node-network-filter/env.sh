904577e0913bab81f37077c4f4c905fcde4b4a80
Tullio Sebastiani <tsebastiani@users.noreply.github.com>
2025-05-02 17:16:38 +0100

    Added Node network filter scripts (#250)
    * introduce node network filter scenario
    
    * build.sh update
    
    renamed yaml file
    
    dco
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    * documentation
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    * docker compose
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    * made run.sh executable
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    * fixed variables
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    ---------
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

---
+++ node-network-filter/env.sh
@@ -0,0 +1,11 @@
+export TEST_DURATION=$TOTAL_CHAOS_DURATION
+export LABEL_SELECTOR=${NODE_SELECTOR:="node-role.kubernetes.io/worker="}
+export NAMESPACE=${NAMESPACE:="default"}
+export INSTANCE_COUNT=${INSTANCE_COUNT:="1"}
+export EXECUTION=${EXECUTION:="parallel"}
+export INGRESS=${INGRESS:="false"}
+export EGRESS=${EGRESS:="true"}
+export INTERFACES=${INTERFACES:=""}
+export PORTS=${PORTS:=""}
+export SCENARIO_TYPE=${SCENARIO_TYPE:=network_chaos_ng_scenarios}
+export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/kube/network-filter.yml}
---
+++ node-network-filter/run.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+ROOT_FOLDER="/home/krkn"
+KRAKEN_FOLDER="$ROOT_FOLDER/kraken"
+SCENARIO_FOLDER="$KRAKEN_FOLDER/scenarios/kube"
+
+# Source env.sh to read all the vars
+source $ROOT_FOLDER/main_env.sh
+source $ROOT_FOLDER/env.sh
+source $ROOT_FOLDER/common_run.sh
+
+if [[ $KRKN_DEBUG == "True" ]];then
+  set -ex
+fi
+
+yq -i ".[0].wait_duration=$TEST_DURATION" $SCENARIO_FOLDER/network-filter.yml
+yq -i ".[0].label_selector=\"$LABEL_SELECTOR\"" $SCENARIO_FOLDER/network-filter.yml
+yq -i ".[0].namespace=\"$NAMESPACE\"" $SCENARIO_FOLDER/network-filter.yml
+yq -i ".[0].instance_count=$INSTANCE_COUNT" $SCENARIO_FOLDER/network-filter.yml
+yq -i ".[0].execution=\"$EXECUTION\"" $SCENARIO_FOLDER/network-filter.yml
+yq -i ".[0].ingress=\"$INGRESS\"" $SCENARIO_FOLDER/network-filter.yml
+yq -i ".[0].egress=\"$EGRESS\"" $SCENARIO_FOLDER/network-filter.yml
+
+IFS=',' read -ra array <<< "$INTERFACES"
+
+for ((i=0; i<${#array[@]}; i++)); do
+  yq -i ".[0].interfaces[$i]=\"${array[$i]}\"" $SCENARIO_FOLDER/network-filter.yml
+done
+
+IFS=',' read -ra array <<< "$PORTS"
+
+for ((i=0; i<${#array[@]}; i++)); do
+  yq -i ".[0].ports[$i]=${array[$i]}" $SCENARIO_FOLDER/network-filter.yml
+done
+
+envsubst < $KRAKEN_FOLDER/config/config.yaml.template > $KRAKEN_FOLDER/config/network-filter-config.yaml
+
+checks
+
+cd $KRAKEN_FOLDER
+
+if [[ $KRKN_DEBUG == "True" ]];then
+  cat $SCENARIO_FOLDER/network-filter.yml
+  cat $KRAKEN_FOLDER/config/network-filter-config.yaml
+fi
+
+
+python3.9 run_kraken.py --config=$KRAKEN_FOLDER/config/network-filter-config.yaml
