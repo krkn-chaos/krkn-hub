e8f6a3aca6604be5f58d6e45a904fc2b1f51a95c
Paige Patton <prubenda@redhat.com>
2025-05-07 14:55:57 -0400

    taking out kubectl commands and no quotes on health checks
    Signed-off-by: Paige Patton <prubenda@redhat.com>

--- common_run.sh
+++ common_run.sh
@@ -37,20 +37,7 @@ check_cluster_version() {
   kubectl get clusterversion || log "Not an OpenShift environment"
 }
 
-# sets kubernetes distribution in krkn config if platform is kubernetes included automatically
-# on all the builds to keep krkn compatible with both the platform
-# called in the checks method below
-
-set_kubernetes_platform() {
-  if ! kubectl get clusterversion;
-  then
-    yq -i '.kraken.distribution="kubernetes"' /home/krkn/kraken/config/config.yaml.template
-  fi
-}
 
 checks() {
   check_oc
-  check_kubectl
-  check_cluster_version
-  set_kubernetes_platform
 }
