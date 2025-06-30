3c6b52feab6dc4849bbe0d7a107bfcd26e90ac31
Tullio Sebastiani <tsebasti@redhat.com>
2025-06-30 09:52:31 +0200

    updated build files
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 SCENARIOS=( application-outages container-scenarios network-chaos node-cpu-hog node-io-hog \
 node-memory-hog node-scenarios node-scenarios-bm pod-network-chaos pod-scenarios power-outages pvc-scenario \
-service-disruption-scenarios service-hijacking syn-flood time-scenarios zone-outages node-network-filter )
+service-disruption-scenarios service-hijacking syn-flood time-scenarios zone-outages node-network-filter pod-network-filter )
 for i in "${SCENARIOS[@]}"; do
     export KRKNCTL_INPUT=$(cat $i/krknctl-input.json|tr -d "\n")
     envsubst < $i/Dockerfile.template > $i/Dockerfile
