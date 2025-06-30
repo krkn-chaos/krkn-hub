93392387245f281fcae576dd63476fabe80d5587
Tullio Sebastiani <tsebasti@redhat.com>
2025-06-30 09:49:14 +0200

    update template files
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- node-network-filter/env.sh
+++ node-network-filter/env.sh
@@ -10,3 +10,5 @@ export PORTS=${PORTS:=""}
 export SCENARIO_TYPE=${SCENARIO_TYPE:=network_chaos_ng_scenarios}
 export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/kube/network-filter.yml}
 export IMAGE=${IMAGE:="quay.io/krkn-chaos/krkn-network-chaos:latest"}
+export PROTOCOLS=${PROTOCOLS:="tcp"}
+export TARGET=${TARGET:=""}
