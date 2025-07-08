2bf5a0fefebd1c8f2db528a9765d5e87def0aa2b
Tullio Sebastiani <tsebastiani@users.noreply.github.com>
2025-07-08 12:54:36 +0200

    removed default node selector from env.sh (#275)
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- node-network-filter/env.sh
+++ node-network-filter/env.sh
@@ -1,5 +1,5 @@
 export TEST_DURATION=$TOTAL_CHAOS_DURATION
-export LABEL_SELECTOR=${NODE_SELECTOR:="node-role.kubernetes.io/worker="}
+export LABEL_SELECTOR=${NODE_SELECTOR:=""}
 export NAMESPACE=${NAMESPACE:="default"}
 export INSTANCE_COUNT=${INSTANCE_COUNT:="1"}
 export EXECUTION=${EXECUTION:="parallel"}
