09d87bb5cc641e5f984ae3f4a2af15a2be2500f5
Tullio Sebastiani <tsebasti@redhat.com>
2025-08-06 14:43:36 +0200

    pod ingress shaping
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

---
+++ pod-ingress-shaping/README.md
@@ -0,0 +1,3 @@
+# Network Chaos docs
+
+See [doc](../docs/pod-ingress-shaping.md) for how to run and all the variables listed
---
+++ pod-ingress-shaping/env.sh
@@ -0,0 +1,15 @@
+export SCENARIO_TYPE=${SCENARIO_TYPE:=network_chaos_ng_scenarios}
+export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/kube/pod-ingress-shaping.yml}
+
+export IMAGE=${IMAGE:="docker.io/fedora/tools"}
+export TEST_DURATION=${TOTAL_CHAOS_DURATION:="60"}
+export POD_SELECTOR=${POD_SELECTOR:=""}
+export EXECUTION=${EXECUTION:="parallel"}
+export NAMESPACE=${NAMESPACE:="default"}
+export INSTANCE_COUNT=${INSTANCE_COUNT:="1"}
+export POD_NAME=${POD_NAME:=""}
+
+export LATENCY=${LATENCY:=""}
+export LOSS=${LOSS:=""}
+export BANDWIDTH=${BANDWIDTH:=""}
+export NETWORK_SHAPING_EXECUTION=${NETWORK_SHAPING_EXECUTION:="parallel"}
