90d42c9dfbd9642217c231fcfaa14a2de74a043e
Tullio Sebastiani <tsebasti@redhat.com>
2025-08-06 14:48:36 +0200

    sorted pod-network-filter variables to align with the krkn source order
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- pod-network-filter/env.sh
+++ pod-network-filter/env.sh
@@ -1,16 +1,21 @@
+export SCENARIO_TYPE=${SCENARIO_TYPE:=network_chaos_ng_scenarios}
+export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/kube/pod-network-filter.yml}
+
+export IMAGE=${IMAGE:="quay.io/krkn-chaos/krkn-network-chaos:latest"}
 export TEST_DURATION=${TOTAL_CHAOS_DURATION:="60"}
 export POD_SELECTOR=${POD_SELECTOR:=""}
+export EXECUTION=${EXECUTION:="parallel"}
 export NAMESPACE=${NAMESPACE:="default"}
 export INSTANCE_COUNT=${INSTANCE_COUNT:="1"}
-export EXECUTION=${EXECUTION:="parallel"}
+export POD_NAME=${POD_NAME:=""}
+
+export SERVICE_ACCOUNT=${SERVICE_ACCOUNT:=""}
+export TAINTS=${TAINTS:=""}
 export INGRESS=${INGRESS:="false"}
 export EGRESS=${EGRESS:="false"}
 export INTERFACES=${INTERFACES:=""}
-export PORTS=${PORTS:=""}
-export SCENARIO_TYPE=${SCENARIO_TYPE:=network_chaos_ng_scenarios}
-export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/kube/network-filter.yml}
-export IMAGE=${IMAGE:="quay.io/krkn-chaos/krkn-network-chaos:latest"}
 export PROTOCOLS=${PROTOCOLS:="tcp"}
-export POD_NAME=${POD_NAME:=""}
-export TAINTS=${TAINTS:=""}
-export SERVICE_ACCOUNT=${SERVICE_ACCOUNT:=""}
+export PORTS=${PORTS:=""}
+
+
+
