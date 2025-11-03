83f2c47d5cc02ff7eedfb5749ce4935a5a18500f
Paige Patton <64206430+paigerube14@users.noreply.github.com>
2025-11-03 12:23:57 -0500

    adding kill count (#297)
    Signed-off-by: Paige Patton <prubenda@redhat.com>

--- kubevirt-outage/env.sh
+++ kubevirt-outage/env.sh
@@ -4,5 +4,6 @@
 export NAMESPACE=${NAMESPACE:=""}
 export VM_NAME=${VM_NAME:=""}
 export TIMEOUT=${TIMEOUT:=60}
+export KILL_COUNT=${KILL_COUNT:=1}
 export SCENARIO_TYPE=${SCENARIO_TYPE:=kubevirt_vm_outage}
 export SCENARIO_FILE=${SCENARIO_FILE:=scenarios/kubevirt_scenario.yaml}
