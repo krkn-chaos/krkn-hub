a5180431be7035b267d4d2a705f28b0e83b12fc8
Tullio Sebastiani <tsebasti@redhat.com>
2026-03-20 15:27:32 +0100

    added the disable resiliency variable
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- env.sh
+++ env.sh
@@ -77,4 +77,5 @@ export KUBE_VIRT_EXIT_ON_FAIL=${KUBE_VIRT_EXIT_ON_FAIL:False}
 # resiliency score
 export RESILIENCY_RUN_MODE=${RESILIENCY_RUN_MODE:="standalone"}
 export RESILIENCY_RUN_MODE=$($RESILIENCY_SCORE  && echo "detailed" || echo "standalone")
+export RESILIENCY_RUN_MODE=$($DISABLE_RESILIENCY_SCORE  && echo "disabled" || echo $RESILIENCY_RUN_MODE)
 export RESILIENCY_FILE=${RESILIENCY_FILE:="config/alerts.yaml"}
